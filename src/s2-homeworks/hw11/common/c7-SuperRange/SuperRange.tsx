e66cd8cbff16f48a0a036801ba728e626aedce4a
Artur Eshbayeu <a.eshbayeu@gmail.com>
2025-11-24 14:26:13 +0100

    Merge remote-tracking branch 'origin/master'

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -5,7 +5,30 @@ const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
-                
+                width: 200,
+                color: "#00CC22",
+                height: 4,
+
+                "& .MuiSlider-thumb": {
+                    width: 18,
+                    height: 18,
+                    backgroundColor: "#FFF",
+                    border: "2px solid #00CC22",
+                    boxShadow: "none",
+
+                    "&:hover": {
+                        boxShadow: "0 0 0 6px rgba(0, 204, 34, 0.16)"
+                    },
+                },
+                '& .MuiSlider-rail': {
+                    backgroundColor: '#8B8B8B', // серый задний трек
+                    opacity: 1,
+                    height: 4,
+                },
+                '& .MuiSlider-track': {
+                    backgroundColor: '#00CC22',
+                    height: 4,
+                }
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
